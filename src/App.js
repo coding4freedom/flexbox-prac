8dea41629df5700b55ac0987f2db22990d3f52a4
Chris <chawkins2010@hotmail.com>
2021-04-20 20:44:14 -0500

    flex direction, flex wrap, flex flow

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="item">Flex item 1</div>
+      <div className="item">Flex item 2</div>
+      <div className="item">Flex item 3</div>
+      <div className="item">Flex item 4</div>
+      <div className="item">Flex item 5</div>
     </div>
   );
 }
