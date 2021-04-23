4b3ccef5e5d0908e35d4361a34cc398edf715c12
Chris <chawkins2010@hotmail.com>
2021-04-22 21:45:03 -0500

    flex-grow, flex-shrink, flex-basis, flex

---
+++ src/FlexItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export default function FlexItems() {
+    return (
+        <div className="container_one">
+            <div className="item one">Flex item 1</div>
+            <div className="item two">Flex item 2</div>
+            <div className="item three">Flex item 3</div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import FlexItems from './FlexItems';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FlexItems />
   </React.StrictMode>,
   document.getElementById('root')
 );
